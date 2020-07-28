9c7b64b23ac69cee3a34b36d077638ba1527cfba
j3ffh95 <j3ffh95@gmail.com>
2020-07-28 15:41:47 -0400

    Finalised with the shuffle function

--- main.js
+++ main.js
@@ -50,4 +50,11 @@ function resetBoard() {
   ];
 }
 
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
 cards.forEach((card) => card.addEventListener("click", flipCard));
