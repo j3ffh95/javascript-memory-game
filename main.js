e4abe4ec7c23583231c32cf23dbda1c575918d7e
j3ffh95 <j3ffh95@gmail.com>
2020-07-28 15:32:34 -0400

    refactor code

--- main.js
+++ main.js
@@ -1,33 +1,43 @@
 const cards = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
   this.classList.add("flip");
 
   if (!hasFlippedCard) {
     // first click
     hasFlippedCard = true;
     firstCard = this;
-  } else {
-    // second click
-    hasFlippedCard = false;
-    secondCard = this;
-
-    // do cards match?
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-      // it's a match!
-      firstCard.removeEventListener("click", flipCard);
-      secondCard.removeEventListener("click", flipCard);
-    } else {
-      // not a match
-      setTimeout(() => {
-        firstCard.classList.remove("flip");
-        secondCard.classList.remove("flip");
-      }, 1500);
-    }
+    return;
   }
+  // second click
+  hasFlippedCard = false;
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disabledCards() : unflipCards();
+}
+
+function disabledCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    lockBoard = false;
+  }, 1500);
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
